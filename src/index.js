ef542ded5f55dc15987c76634108428d9f7f9895
Erik Konner <erikkonner@gmail.com>
2019-07-08 18:50:51 -0600

    created store and imported redux context in index.js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { rootReducer } from './reducers';
+import thunk from 'redux-thunk';
 import './styles/index.scss';
 import App from './components/app/App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
 const router = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 
 )
 
