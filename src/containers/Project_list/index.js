9d410a27bdee286ffcd345511080142e964b3f22
RyanDBarnett <conner2468@gmail.com>
2019-07-09 22:16:46 -0600

    Resolve merge conflict in Project_list

--- src/containers/Project_list/index.js
+++ src/containers/Project_list/index.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Project from '../../components/Project/index';
+import { addProject } from '../../actions';
+import './Project_list.scss';
 
 class Project_list extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: ""
+    }
+  }
+
+  handleChange = (e) => {
+    const name = e.target.value;
+    this.setState({ name })
+  }
+
   assignProjects = () => {
    let { projects } = this.props;
    let displayProjects;
    
-
    if (projects && projects.length) {
      displayProjects = projects.map(project => 
        <Project {...project} key={project.id} />
@@ -19,13 +32,15 @@ class Project_list extends Component {
    }
    return displayProjects;
  }
+
  render() {
-   const displayProjects = this.assignProjects();
-   return (
-     <div className="project-container">
-       {displayProjects}
-     </div>
-   )
+    const displayProjects = this.assignProjects();
+    return (
+      <div className="project-container">
+        <input type="text" onChange={this.handleChange()} placeholder="Project Name" value={this.state.name} />
+        {displayProjects}
+      </div>
+    )
  }
 
 }
@@ -36,7 +51,7 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-
+  addProject: (project) => dispatch(addProject(project))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Project_list);
