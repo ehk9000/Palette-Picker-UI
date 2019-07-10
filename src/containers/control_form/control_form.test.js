f7c7ba2d80e09a21dfe62254e3aa41dea99be970
Erik Konner <erikkonner@gmail.com>
2019-07-10 17:20:21 -0600

    Add All Palettes thunk fully tested. added props to two components connected to store to give context

--- src/containers/control_form/control_form.test.js
+++ src/containers/control_form/control_form.test.js
@@ -4,9 +4,15 @@ import { ControlForm, mapStateToProps, mapDispatchToProps } from './index';
 
 describe('ControlForm', () => {
   let wrapper;
+  let mockProjects = {id:1, name:'Test'}
+  let mockPalettes = {id:1, color_1:"SDFH", name:'wave'}
+  let mockCurrentPalette = { id: 1, color_1: "SDFH", name: "wave" }
 
   beforeEach(() => {
-    wrapper = shallow( <ControlForm/> )
+    wrapper = shallow( <ControlForm 
+      projects={mockProjects} 
+      palettes={mockPalettes}
+      currentPalette={mockCurrentPalette}/> )
   });
 
   it('Should render correctly', () => {
